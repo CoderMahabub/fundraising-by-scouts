c69bb6e78aaa34ace59d63cfeba3c9f869886bb4
CoderMahabub <codermahabub@gmail.com>
2021-09-26 05:15:41 +0600

    Header Added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <header className="header">
+                <h1>Fundraising By Scouts 4 Unprevileged Children</h1>
+                <p>If you make the world better for KIDS, you make it better for EVERYONE. -<quotation>Kid President</quotation></p>
+                <h3>Target Fund: <span>1000000</span> ৳</h3>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/ScoutMain/ScoutMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ScoutMain.css';
+
+const ScoutMain = () => {
+    return (
+        <div>
+            <h1>Scout Maain</h1>
+        </div>
+    );
+};
+
+export default ScoutMain;
