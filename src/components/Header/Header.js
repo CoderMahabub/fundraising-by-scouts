9619e2d4d32ee6237631d45a97f2a71ba0acfd4b
CoderMahabub <codermahabub@gmail.com>
2021-09-26 15:48:26 +0600

    Github Pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div>
             <header className="header">
-                <h1>Fundraising By Scouts 4 Unprevileged Children</h1>
+                <h1>Fundraising By Scouts For Unprevileged Children</h1>
                 <p>If you make the world better for KIDS, you make it better for EVERYONE. -<i>Kid President</i></p>
                 <h3>Target Fund: <span style={{ color: 'orange', fontSize: '24px' }}>$1000000</span></h3>
             </header>
